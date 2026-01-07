6bdf7093df63369d3164881a2f7381fb2aa587c1
Baptiste Faroy <baptiste.faroy@epita.fr>
2026-01-08 00:24:54 +0100

    feat: ast created for step 1

--- src/ast/ast.c
+++ src/ast/ast.c
@@ -1,6 +1,115 @@
 #include "ast.h"
 
-void ast_init(void)
+#include <sdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static ast_t *ast_init(ast_type_t type)
 {
-    // TODO: implement
+    ast_t *new = calloc(1, sizeof(*new));
+    if (!new)
+    {
+        return NULL;
+    }
+
+    new->type = type;
+    return new;
+}
+
+ast_t *ast_if_init(ast_t *condition, ast_t *then_body, ast_t *else_body)
+{
+    ast_t *new = ast_init(AST_IF) if (!new)
+    {
+        return NULL;
+    }
+
+    new->data.ast_if.condition = condition;
+    new->data.ast_if.then_body = then_body;
+    new->data.ast_if.else_body = else_body;
+
+    return new;
+}
+
+ast_t *ast_cmd_init(ast_t **agrv)
+{
+    ast_t *new = ast_init(AST_COMMAND);
+    if (!new)
+    {
+        return NULL;
+    }
+
+    new->data.ast_cmd.argv = argv;
+
+    return new;
+}
+
+ast_t *ast_list_init(ast_t *next, ast_t *child)
+{
+    ast_t *new = ast_init(AST_LIST);
+    if (!new)
+    {
+        return NULL;
+    }
+
+    new->data.ast_list.next = next;
+    new->data.ast_list.child = child;
+
+    return new;
+}
+
+ast_t *ast_pipe_init(ast_t *right, ast_t *left)
+{
+    ast_t *new = ast_init(AST_PIPE);
+    if (!new)
+    {
+        return NULL;
+    }
+
+    new->data.ast_pipe.right = right;
+    new->data.ast_pipe.left = left;
+
+    return new;
+}
+
+static void argv_free(char **argv)
+{
+    if (!argv)
+    {
+        return;
+    }
+
+    for (size_t i = 0; argv[i]; i++)
+    {
+        free(argv[i]);
+    }
+    free(argv);
+}
+
+void ast_free(ast_t *node)
+{
+    if (!node)
+    {
+        return;
+    }
+
+    switch (node->type)
+    {
+    case AST_CMD:
+        agrv_free(node->ast_cmd.argv);
+        break;
+
+    case AST_IF:
+        ast_free(node->data.ast_if.condition);
+        ast_free(node->data.ast_if.then_body);
+        ast_free(node->data.ast_if.else_body);
+        break;
+
+    case AST_LIST:
+        ast_free(node->data.ast_list.next);
+        ast_free(node->data.ast_list.current);
+        break;
+    default:
+        break;
+    }
+    free(node);
 }
--- src/ast/ast.h
+++ src/ast/ast.h
@@ -1,24 +1,84 @@
 #ifndef AST_H
 #define AST_H
+
+#include <stdio.h>
+
 /* transform tokens into AST nodes */
-enum node_type 
+
+// !! Variables / Pipe / Redirect for STEP 2 !!
+
+// input = list '\n' | list EOF | '\n' | EOF;
+// list = and_or;
+// and_or = pipeline;
+// pipeline = command;
+// command = simple_command;
+// simple_command = WORD { element };
+// element = WORD;
+
+typedef enum ast_type
 {
-    NODE_COMMAND,
-    NODE_PIPE,
-    NODE_IF,
-    NODE_AND,
-    NODE_REDIRECT
+    AST_INPUT,
+    AST_LIST,
+    AST_AND_OR,
+    AST_PIPELINE,
+    AST_COMMAND,
+    AST_SIMPLE_COMMAND,
+    AST_ELEMENT,
+    AST_IF
+} ast_type_t;
+
+struct ast_if
+{
+    struct ast *condition; // the condition
+    struct ast *then_body; // the body of the then clause
+    struct ast *else_body; // the body of the else, may be NULL
 };
 
-struct ast // ex for: echo toto tata > file.txt
+struct ast_cmd // part with multiple commands
 {
-    enum node_type type;  // NODE_COMMAND
-    char **args;        // Simple command ex: = [echo], [toto], [tata], NULL
-    struct ast *left;   // If condition / Left part of pipe etc
-    struct ast *right;  // If body / Right part of Pipe etc
-    struct ast *else_branch; // NULL
-    char *redir_file; // file.txt
-    int  redir_fd; // 1 (stdout)
+    char **argv; // list of args
+}; // no need to have a size as argv finish with NULL
+
+struct ast_input;
+
+struct ast_list
+{
+    struct ast *next;
+    struct ast *child;
 };
+
+struct ast_pipe
+{
+    struct ast *right;
+    struct ast *left;
+};
+
+struct ast_redir;
+/*{
+    struct ast *next;
+    FILE *redirect;
+};*/
+
+union ast_union
+{
+    struct ast_cmd ast_cmd;
+    struct ast_if ast_if;
+    struct ast_list ast_list;
+    struct ast_pipe ast_pipe;
+    struct ast_redir ast_redir;
+};
+
+typedef struct ast
+{
+    ast_type_t type;
+    union ast_union data;
+} ast_t;
+
+ast_t *ast_if_init(ast_t *condition, ast_t *then_body, ast *else_body);
+ast_t *ast_cmd_init(ast_t **agrv);
+ast_t *ast_list_init(ast_t *next, ast_t *child);
+ast_t *ast_pipe_init(ast_t *right, ast_t *left);
+
+void ast_free(ast_t *node);
 
 #endif /* ! AST_H */
