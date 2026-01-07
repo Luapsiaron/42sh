7d3263a4078521405a433deafc03bf67d4307cfa
Baptiste Faroy <baptiste.faroy@epita.fr>
2026-01-07 17:41:41 +0100

    feat: add io backend to transform string and stdin into temporary file

---
+++ src/io/io.c
@@ -0,0 +1,36 @@
+#include "io.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+FILE *io_string_to_file(char *string)
+{
+    FILE *temporary_file = tmpfile();
+    if (!temporary_file)
+    {
+        return NULL;
+    }
+
+    fprintf(temporary_file, "%s", string);
+
+    return temporary_file;
+}
+
+FILE *io_stdin_to_file(void)
+{
+    FILE *temporary_file = tmpfile();
+    if (!temporary_file)
+    {
+        return NULL;
+    }
+
+    char buffer[4096];
+    ssize_t size_readed;
+    while ((size_readed = read(STDIN_FILENO, &buffer, sizeof(buffer))) > 0)
+    {
+        fprintf(temporary_file, "%s", buffer);
+    }
+
+    return temporary_file;
+}
--- src/io/io.h
+++ src/io/io.h
@@ -1,1 +1,12 @@
-/* transform strings into files */
+#ifndef IO_H
+#define IO_H
+
+#include <stdio.h>
+
+// Take a string and return a temporary file with the string in it
+FILE *io_string_to_file(char *string);
+
+// Take standard inout to put it into a temporary file
+FILE *io_stdin_to_file(void);
+
+#endif /* ! IO_H */
