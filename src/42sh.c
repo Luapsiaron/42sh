728d49b52a3dbb528b398462a8e8e526c1c3dae6
thibaut.dupre <thibaut.dupre@epita.fr>
2026-01-17 20:14:25 +0100

    fix: main line limit

--- src/42sh.c
+++ src/42sh.c
@@ -12,6 +12,13 @@
 #include "utils/parser/pretty_printer.h"
 #include "expansion/hashmap.h"
 
+struct input_sel_ctx {
+    int argc;
+    char **argv;
+    char *command;
+    struct hash_map *hm;
+};
+
 static void usage(FILE *out)
 {
     fprintf(out,
@@ -54,94 +61,76 @@ static int run_stream(FILE *input, int pretty_print, struct hash_map *hm)
     return status;
 }
 
-int main(int argc, char **argv, char **envp) // envp -> collect system variables
+static void init_env_hashmap(struct hash_map *hm, char **envp)
 {
-    int opt;
-    char *command = NULL;
-    int pretty_print = 0;
-
-    struct hash_map *hm = hash_map_init(100); // 100 = magic value
-
-    for(int i = 0; envp[i]; i++)
+    for (int i = 0; envp[i]; i++)
     {
         char *eq = strchr(envp[i], '=');
-        if(eq)
-        {
-            *eq = '\0';
-            hash_map_insert(hm, envp[i], eq + 1, NULL);
-            *eq = '=';
-        }
+        if (!eq)
+            continue;
+        *eq = '\0';
+        hash_map_insert(hm, envp[i], eq + 1, NULL);
+        *eq = '=';
     }
+}
 
-    if (getenv("PRETTY_PRINT") != NULL)
-    {
-        pretty_print = 1;
-    }
+static FILE *select_input_stream(struct input_sel_ctx c, bool *must_close)
+{
+    int remaining = c.argc - optind;
+    *must_close = false;
 
-    for (int i = 1; i < argc; ++i)
-    {
-        if (strcmp(argv[i], "--pretty-print") == 0)
-        {
-            pretty_print = 1;
-            for (int j = i; j < argc - 1; ++j)
-            {
-                argv[j] = argv[j + 1];
-            }
-            --argc;
-            --i;
-        }
-    }
-
-    while ((opt = getopt(argc, argv, "c:")) != -1)
-    {
-        switch (opt)
-        {
-        case 'c':
-            command = optarg;
-            break;
-        case '?':
-            if (optopt == 'c')
-                error_usage("option -c requires an argument");
-            error_usage("invalid option");
-            break;
-        default:
-            error_usage("invalid option");
-        }
-    }
-    int remaining = argc - optind;
-    FILE *input = NULL;
-    bool must_close = false;
-    if (command != NULL)
+    if (c.command != NULL)
     {
         if (remaining > 0)
             error_usage("cannot use -c with a script file operand");
-        input = io_string_to_file(command);
-        if (!input)
+        FILE *in = io_string_to_file(c.command);
+        if (!in)
             error_usage("failed to open script string");
-        must_close = true;
+        *must_close = true;
+        return in;
     }
-    else if (remaining >= 1)
+
+    if (remaining >= 1)
     {
-        const char *path = argv[optind];
-        input = fopen(path, "r");
-        if (!input)
+        const char *path = c.argv[optind];
+        FILE *in = fopen(path, "r");
+        if (!in)
         {
-            fprintf(stderr, "42sh: cannot open '%s': %s\n", path,
-                    strerror(errno));
-            hash_map_free(hm);
+            fprintf(stderr, "42sh: cannot open '%s': %s\n", path, strerror(errno));
+            hash_map_free(c.hm);
             exit(2);
         }
-        must_close = true;
+        *must_close = true;
+        return in;
     }
-    else
-    {
-        input = stdin;
-        must_close = false;
-    }
-    int status = run_stream(input, pretty_print, hm);
-    if (must_close && input)
-        fclose(input);
+    return stdin;
+}
 
+int main(int argc, char **argv, char **envp)
+{
+    int opt, pretty_print = 0;
+    char *command = NULL;
+    struct hash_map *hm = hash_map_init(100);
+
+    init_env_hashmap(hm, envp);
+    if (getenv("PRETTY_PRINT") != NULL)
+        pretty_print = 1;
+
+    for (int i = 1; i < argc; ++i)
+        if (strcmp(argv[i], "--pretty-print") == 0)
+        { pretty_print = 1; for (int j = i; j < argc - 1; ++j) argv[j] = argv[j + 1]; --argc; --i; }
+
+    while ((opt = getopt(argc, argv, "c:")) != -1)
+        if (opt == 'c') command = optarg;
+        else if (opt == '?' && optopt == 'c') error_usage("option -c requires an argument");
+        else error_usage("invalid option");
+
+    bool must_close = false;
+    struct input_sel_ctx c = { argc, argv, command, hm };
+    FILE *input = select_input_stream(c, &must_close);
+
+    int status = run_stream(input, pretty_print, hm);
+    if (must_close && input) fclose(input);
     hash_map_free(hm);
     return status;
 }
