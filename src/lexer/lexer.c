cd2d67d7bcf655e48dcaf13b0d461b4693b519ac
Paul Norais <paul.norais@epita.fr>
2026-01-17 19:05:40 +0100

    feat: lexer single and double quotes

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -61,23 +61,139 @@ static int is_assignment_word(const char *s)
     return s[i] == '=';
 }
 
+static int append_buffer(char *buffer, size_t *index, size_t capacity, int c)
+{
+    if (*index >= capacity - 1)
+    {
+        return 0; 
+    }
+    buffer[(*index)++] = (char)c;
+    return 1;
+}
+
+static int lexer_single_quotes(lexer_t *lx, char *buffer, size_t *index, size_t capacity)
+{
+    if(!append_buffer(buffer, index, capacity, '\''))
+    {
+        return 0;
+    }
+    lexer_next_char(lx);
+
+    while (lx->current != EOF && lx->current != '\'')
+    {
+        if (!append_buffer(buffer, index, capacity, lx->current))
+        {
+            return 0;
+        }
+        lexer_next_char(lx);
+    }
+
+    if (lx->current != '\'')
+    {
+        return 0; 
+    }
+
+    if(!append_buffer(buffer, index, capacity, '\''))
+    {
+        return 0;
+    }
+    lexer_next_char(lx);
+    return 1;
+}
+
+static int lexer_double_quotes(lexer_t *lx, char *buffer, size_t *index, size_t capacity)
+{
+    if(!append_buffer(buffer, index, capacity, '"'))
+    {
+        return 0;
+    }
+    lexer_next_char(lx);
+
+    while (lx->current != EOF && lx->current != '"')
+    {
+        if(lx->current == '\\')
+        {
+            lexer_next_char(lx);
+            if (lx->current == EOF)
+            {
+                return 0; 
+            }
+
+            if(lx->current == '\n')
+            {
+                lexer_next_char(lx);
+                continue; 
+            }
+
+            if(!append_buffer(buffer, index, capacity, '\\'))
+            {
+                return 0; 
+            }
+            if(!append_buffer(buffer, index, capacity, lx->current))
+            {
+                return 0;
+            }
+            lexer_next_char(lx);
+            continue;
+        }
+        if (!append_buffer(buffer, index, capacity, lx->current))
+        {
+            return 0; 
+        }
+        lexer_next_char(lx);
+    }
+
+    if (lx->current != '"')
+    {
+        return 0; 
+    }
+    if(!append_buffer(buffer, index, capacity, '"'))
+    {
+        return 0;
+    }
+
+    lexer_next_char(lx);
+    return 1;
+}
+
 static token_t *lexer_is_word(lexer_t *lx)
 {
     char buffer[512];
     size_t i = 0;
+    int quote = 0;
 
     while (lx->current != EOF && lx->current != ';' && lx->current != '|'
            && lx->current != '>' && lx->current != '<' && lx->current != '\n'
            && !isspace(lx->current))
     {
-        buffer[i++] = lx->current;
+        if (lx->current == '\'')
+        {
+            quote = 1;
+            if (!lexer_single_quotes(lx, buffer, &i, sizeof(buffer)))
+            {
+                return NULL; 
+            }
+            continue;
+        }
+        if (lx->current == '"')
+        {
+            quote = 1;
+            if (!lexer_double_quotes(lx, buffer, &i, sizeof(buffer)))
+            {
+                return NULL; 
+            }
+            continue;
+        }
+        if (!append_buffer(buffer, &i, sizeof(buffer), lx->current))
+        {
+            return NULL; 
+        }
         lexer_next_char(lx);
     }
     buffer[i] = '\0';
 
     if (is_assignment_word(buffer))
     {
-        lx->condition = LEXER_WORD_UNTIL;
         return token_new(TOKEN_ASSIGNMENT_WORD, buffer);
     }
 
@@ -87,11 +203,10 @@ static token_t *lexer_is_word(lexer_t *lx)
         return token_new(TOKEN_WORD, buffer);
     }
 
-    if (lx->condition == LEXER_NORMAL)
+    token_type_t type;
+    if (!quote && lx->condition == LEXER_NORMAL && token_is_reserved_word(buffer, &type))
     {
-        token_type_t type;
-        if (token_is_reserved_word(buffer, &type))
-            return token_new(type, NULL);
+        return token_new(type, NULL);
     }
 
     lx->condition = LEXER_WORD_UNTIL;
